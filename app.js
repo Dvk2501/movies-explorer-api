a498f93585be300b91d4005854c425c753882b71
Dmitry Kravchenko <dmitry.kravchenko96@yandex.ru>
2023-03-01 15:01:58 +0300

    1

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const helmet = require('helmet');
+const { errors } = require('celebrate');
+const handlerErrors = require('./middlewares/handleError');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+const router = require('./routes');
+
+const { PORT = 3000 } = process.env;
+const app = express();
+mongoose.connect('mongodb://localhost:27017/bitfilmsdb ');
+
+app.use(express.json());
+
+app.use(helmet());
+app.use(requestLogger);
+
+app.use(router);
+app.use(errorLogger);
+app.use(errors());
+app.use(handlerErrors);
+
+app.listen(PORT);
